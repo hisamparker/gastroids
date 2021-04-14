c7d50dada9ea9a090bf0431b330c926b2f67274f
hisamparker <hisamparker@gmail.com>
2021-04-14 20:38:41 +0200

    added fart logic :P logic for lose game and velocity for player and future fart

--- classes.js
+++ classes.js
@@ -6,10 +6,11 @@ class GameArea {
         this.frames = 0;
         this.points = 0;
         this.level = 1;
+        this.state = 'playing';
         this.rafId = null;
         this.projectiles = [];
         this.enemies = [];
-        this.player = new Player (this.canvas, this.ctx);
+        this.player = new Player (this.canvas, this.ctx, this);
     }
 
     initGame(){
@@ -23,7 +24,6 @@ class GameArea {
         const pointY = e.clientY -rect.top;
         const pointX = e.clientX - rect.left;
         const angle = Math.atan2(pointY - this.size.h/2, pointX - this.size.w/2);
-        console.log('event', e.pageY);
         const velocity = {
             x:Math.cos(angle),
             y:Math.sin(angle)
@@ -58,51 +58,53 @@ class GameArea {
         this.rafId = requestAnimationFrame(() => this.updateGame());
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
         this.player.drawPlayer();
-        this.frames += 1;
-        this.levelUp();
+        if (this.state === 'playing') {
+            this.frames += 1;
+            this.levelUp();
 
-        this.projectiles.forEach((projectile, index) => {
-            projectile.update();
-            //remove offscreen projectiles
-            if(
-                projectile.pos.x + projectile.size.w < 0 || 
-                projectile.pos.x - projectile.size.w > this.size.w || 
-                projectile.y + projectile.size.h < 0 || 
-                projectile.y - projectile.size.h > this.size.h) {
-                setTimeout(() => {
-                    this.projectiles.splice(index, 1);
-                }, 0);
-            }
-        });
-
-        this.enemies.forEach((enemy, index) => {
-            enemy.update();
-    
-            const distance = Math.hypot(this.player.pos.x - enemy.pos.x, this.player.pos.y - enemy.pos.y); 
-            if (distance - enemy.size.w/2 - this.player.size.w/2 < 1){
-                this.enemies.splice(index, 1);
-                this.player.lives -= 1;
-                trackLives(this.player.lives);
-                if(this.player.lives <= 0) {
-                    cancelAnimationFrame(this.rafId);
-                }
-            }
-            
-            //remove projectiles and enemies if they crash
-            this.projectiles.forEach((projectile, projectileIndex) => {
-                const distance = Math.hypot(projectile.pos.x - enemy.pos.x, projectile.pos.y - enemy.pos.y); {
-                    if (distance - enemy.size.w/2 - projectile.size.w/2 < 1){
-                        this.points += 10;
-                        keepScore(this.points);
-                        console.log(this.points)
-                        setTimeout(() => {
-                            this.enemies.splice(index, 1);
-                            this.projectiles.splice(projectileIndex, 1);
-                        }, 0);
-                    }
+            this.projectiles.forEach((projectile, index) => {
+                projectile.update();
+                //remove offscreen projectiles
+                if(
+                    projectile.pos.x + projectile.size.w < 0 || 
+                    projectile.pos.x - projectile.size.w > this.size.w || 
+                    projectile.y + projectile.size.h < 0 || 
+                    projectile.y - projectile.size.h > this.size.h) {
+                    setTimeout(() => {
+                        this.projectiles.splice(index, 1);
+                    }, 0);
                 }
             });
-        });
+
+            this.enemies.forEach((enemy, index) => {
+                enemy.update();
+        
+                const distance = Math.hypot(this.player.pos.x - enemy.pos.x, this.player.pos.y - enemy.pos.y); 
+                if (distance - enemy.size.w/2 - this.player.size.w/2 < 1){
+                    this.enemies.splice(index, 1);
+                    this.player.lives -= 1;
+                    trackLives(this.player.lives);
+                    if(this.player.lives <= 0) {
+                        this.loseGame();
+                    }
+                }
+                
+                //remove projectiles and enemies if they crash
+                this.projectiles.forEach((projectile, projectileIndex) => {
+                    const distance = Math.hypot(projectile.pos.x - enemy.pos.x, projectile.pos.y - enemy.pos.y); {
+                        if (distance - enemy.size.w/2 - projectile.size.w/2 < 1){
+                            this.points += 10;
+                            keepScore(this.points);
+                            console.log(this.points)
+                            setTimeout(() => {
+                                this.enemies.splice(index, 1);
+                                this.projectiles.splice(projectileIndex, 1);
+                            }, 0);
+                        }
+                    }
+                });
+            });
+        }
     }
 
     levelUp() {
@@ -120,12 +122,16 @@ class GameArea {
     }
 
     winGame(){
-        this.stopGame();
+        //this.stopGame();
+        this.state = 'winning';
 
     }
 
     loseGame(){
-        this.stopGame();
+        //this.stopGame();
+        this.state = 'dying';
+        this.player.vel.y = -4;
+        this.player.fartVel.y = 4;
 
     }
 
@@ -138,20 +144,47 @@ class GameArea {
 }
 
 class Player {
-    constructor(canvas, ctx){
+    constructor(canvas, ctx, gameArea){
+        this.gameArea = gameArea;
         this.canvas = canvas;
         this.ctx = ctx;
         this.size = {w: 64, h: 64};
-        this.pos = {x: this.canvas.width / 2 - this.size.w / 2, y: this.canvas.height / 2 - this.size.h / 2};
-        this.lives = 10;
+        this.fartSize = {w: 74, h: 74};
+        this.vel = {x:0, y:0};
+        this.pos = {x: this.canvas.width / 2, y: this.canvas.height / 2};
+        this.fartVel = {x:0, y:0};
+        this.fartPos = {x: this.canvas.width / 2 - this.size.w / 2, y: this.canvas.height / 2 - this.size.h / 2};
+        this.lives = 2;
         this.eff = new Image();
         this.eff.src = './images/icecream-vendor.png';
         this.winEff = new Image();
-        this.winEff.src = null;
+        this.winEff.src = './images/icecreamshop.png';
+        this.loseEff = new Image();
+        this.loseEff.src = './images/melted-icecream.png'
     }
 
     drawPlayer() {
-        this.ctx.drawImage(this.eff, this.pos.x, this.pos.y, this.size.w, this.size.h);
+        if (this.gameArea.state === 'dying') {
+            this.drawFart();
+            this.pos.y += this.vel.y;
+            this.vel.y *= 0.99; 
+            if (this.vel.y > - 0.5) {
+                this.vel.y = - 0.5
+            }
+            this.fartPos.y += this.fartVel.y;
+            this.fartVel.y *= 0.97;
+            this.fartSize.w += this.fartVel.y;
+            this.fartSize.h += this.fartVel.y;
+        }
+        if (this.gameArea.state === 'winning') {
+            this.ctx.drawImage(this.winEff, this.pos.x - this.size.w/2, this.pos.y - this.size.h/2, this.size.w, this.size.h);
+        } else {
+            this.ctx.drawImage(this.eff, this.pos.x - this.size.w/2, this.pos.y - this.size.h, this.size.w, this.size.h);
+        } 
+    }
+
+    drawFart() {
+        this.ctx.drawImage(this.loseEff, this.fartPos.x - this.fartSize.w/2, this.fartPos.y - this.fartSize.h/2, this.fartSize.w, this.fartSize.h);
     }
 
     checkCollision() {
@@ -217,7 +250,6 @@ class Enemy {
 
     update() {
         this.drawEnemy();
-        console.log('hihihih')
         this.pos.x = this.pos.x + this.vel.x;
         this.pos.y = this.pos.y + this.vel.y;
     }
