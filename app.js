9cb7358bd99d0e7eda032cb19b2422e44a0650d4
hisamparker <hisamparker@gmail.com>
2021-04-16 08:34:04 +0200

    added reset button, you lose img, and you win img

--- app.js
+++ app.js
@@ -3,18 +3,39 @@ let gameArea = null;
 
 if (startButton) {
   startButton.addEventListener('click', e => {
-    const body = document.body;
-    body.classList.add('isPlaying');
-    const main = document.getElementById('main');
-    const canvas = document.createElement('canvas');
-    canvas.setAttribute('id', 'canvas');
-    main.appendChild(canvas);
-    sizeCanvasToParent();
-    gameArea = new GameArea(canvas);
-    gameArea.initGame();
-    document.getElementById('canvas').addEventListener('click', (event) => {
+    if(startButton.classList.contains('reading__1')) {
+      const p1 = document.querySelector('.p_1');
+      const p2 = document.querySelector('.p_2');
+      p1.innerHTML = 'Alas!';
+      p2.innerText = 'The universe is unkind and continually assaults our young hero with deliciously creamy, dairy treats.';
+      startButton.classList.add('reading__2');
+      startButton.classList.remove('reading__1');
+    } else if (startButton.classList.contains('reading__2')) {
+      startButton.innerHTML = 'Play &#127846';
+      const p1 = document.querySelector('.p_1');
+      const p2 = document.querySelector('.p_2');
+      p1.innerHTML = 'Can you help Charlie dodge these temptations, avoid sever stomach bloat, and safe guard the universe from her gastric distress?';
+      p2.innerText = 'Somehow I doubt it, but hey, why not give it a try?';
+      startButton.classList.add('reading__3');
+      startButton.classList.remove('reading__2');
+    } else if (startButton.classList.contains('reading__3')) {
+      const body = document.body;
+      body.classList.add('isPlaying');
+      const main = document.getElementById('main');
+      const canvas = document.createElement('canvas');
+      canvas.setAttribute('id', 'canvas');
+      main.appendChild(canvas);
+      sizeCanvasToParent();
+      gameArea = new GameArea(canvas);
+      gameArea.initGame();
+      document.getElementById('canvas').addEventListener('click', (event) => {
       gameArea.spawnProjectile(event);
-    });
+      });
+      const resetButton = document.createElement('button');
+      resetButton.setAttribute("id", "reset_button");
+      resetButton.innerHTML = 'Reset';
+      document.getElementById('header').appendChild(resetButton);
+    }
   });
 }
 
@@ -30,14 +51,14 @@ function sizeCanvasToParent () {
 //update canvas size to match main element
 window.onresize = sizeCanvasToParent;
 
-function keepScore (points) {
+function trackScore (points) {
   const score = document.getElementById('score');
   score.innerText = points;
 }
 
 function trackLives (lives) {
-  const charLives = document.getElementById('lives');
-  charLives.innerText = lives;
+  const playerLives = document.getElementById('lives');
+  playerLives.innerText = lives;
 }
 
 function trackLevels (level) {
--- classes.js
+++ classes.js
@@ -1,6 +1,7 @@
 class GameArea {
     constructor(canvas) {
         this.canvas = canvas;
+        //check that canvas is compatible w/browser
         this.ctx = canvas.getContext ? canvas.getContext('2d') : alert('upgrade now bish.');
         this.size = {w: this.canvas.width, h: this.canvas.height};
         this.frames = 0;
@@ -8,22 +9,31 @@ class GameArea {
         this.level = 1;
         this.state = 'playing';
         this.rafId = null;
+        //use arrays to create groupings of elements so that I can render them all at once (instead of one-by-one)
         this.projectiles = [];
         this.enemies = [];
+        this.wins = [];
         this.player = new Player (this.canvas, this.ctx, this);
     }
 
     initGame(){
+        //first request animation frame, only called once on updateGame
         requestAnimationFrame(() => this.updateGame());
-        if (this.state === 'playing') {
-            this.spawnEnemies();
-        }
+        //calls spawn enemies once to kick of the interval
+        this.spawnEnemies();
+        //display levels, lives, and score on start
+        trackLevels(this.level);
+        trackLives(this.player.lives);
+        trackScore(this.points);
 
     }
 
+    //called in app.js, on click, new projectiles are spawned, click event is passed to func to get click position
     spawnProjectile(e) {
+        //getBoundingClientRect() returns a DOMRect object, this gets the size and position of an element's bounding box, relative to the viewport
         const rect = e.target.getBoundingClientRect();
-        const pointY = e.clientY -rect.top;
+        //the event position is relative to the entire viewport, rect.top is equal to the difference btw the top of the vp and the top of the (main)element surrounding my canvas, so I need to subtract that from e.clientY to get click event position relative to my canvas (client left gets the diff btw the left of the vp and the left of my element)
+        const pointY = e.clientY - rect.top;
         const pointX = e.clientX - rect.left;
         const angle = Math.atan2(pointY - this.size.h/2, pointX - this.size.w/2);
         const velocity = {
@@ -57,13 +67,40 @@ class GameArea {
         }, 1000);  
     }
 
+    spawnWins() {
+        setInterval(() => {
+            let pointX, pointY;
+            if (Math.random() < 0.5) {
+                pointX = Math.random() < 0.5 ? 0 - 20 : this.size.w + 20;
+                pointY = Math.random() * this.size.h;
+            } else {
+                pointX = Math.random() * this.size.w;
+                pointY = Math.random() < 0.5 ? 0 - 20 : this.size.h + 20;
+            } 
+
+            const angle = Math.atan2(pointY - this.size.h/2, pointX - this.size.w/2);
+            const velocity = {
+                x:Math.cos(angle),
+                y:Math.sin(angle)
+            };
+            this.wins.push(new Win(this, velocity));
+        }, 10);  
+    }
+
     updateGame(){
         this.rafId = requestAnimationFrame(() => this.updateGame());
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        if (this.state === 'winning') {
+            this.wins.forEach((win) => {
+                win.update();    
+            });
+        }
+        
         this.player.drawPlayer();
     
         this.frames += 1;
-        this.levelUp();
+        if(this.state === 'playing') {this.levelUp();}
 
         this.projectiles.forEach((projectile, index) => {
             projectile.update();
@@ -85,11 +122,13 @@ class GameArea {
             const distance = Math.hypot(this.player.pos.x - enemy.pos.x, this.player.pos.y - enemy.pos.y); 
             if (distance - enemy.size.w/2 - this.player.size.w/2 < 1){
                 this.enemies.splice(index, 1);
-                this.player.lives -= 1;
-                trackLives(this.player.lives);
-                if(this.player.lives <= 0) {
+                if (this.player.lives <= 1) {
                     this.loseGame();
                 }
+                if (this.player.lives > 0) {
+                    this.player.lives -= 1;
+                    trackLives(this.player.lives);
+                } 
             }
             
             //remove projectiles and enemies if they crash
@@ -97,7 +136,7 @@ class GameArea {
                 const distance = Math.hypot(projectile.pos.x - enemy.pos.x, projectile.pos.y - enemy.pos.y); {
                     if (distance - enemy.size.w/2 - projectile.size.w/2 < 1){
                         this.points += 10;
-                        keepScore(this.points);
+                        trackScore(this.points);
                         setTimeout(() => {
                             this.enemies.splice(index, 1);
                             this.projectiles.splice(projectileIndex, 1);
@@ -114,7 +153,7 @@ class GameArea {
             this.level += 1;
             trackLevels(this.level);
         }
-        if (this.level >= 10) {
+        if (this.level >= 2) {
             this.winGame();
         }
     }
@@ -126,7 +165,8 @@ class GameArea {
     winGame(){
         //this.stopGame();
         this.state = 'winning';
-
+        this.spawnWins();
+        
     }
 
     loseGame(){
@@ -160,29 +200,35 @@ class Player {
         this.eff = new Image();
         this.eff.src = './images/eff.png';
         this.winEff = new Image();
-        this.winEff.src = './images/eff-win.png.png';
+        this.winEff.src = './images/eff-win.png';
         this.fart = new Image();
         this.fart.src = './images/fart.png';
+        this.loseTextEff = new Image();
+        this.loseTextEff.src = './images/cheese.png';
     }
 
     drawPlayer() {
         if (this.gameArea.state === 'dying') {
-            this.drawFart();
-            this.pos.y += this.vel.y;
-            this.vel.y *= 0.99; 
-            if (this.vel.y > - 0.5) {
-                this.vel.y = - 0.5;
+            if(this.fartPos.y > 484) {
+                this.drawLoseText();
+            } else {
+                this.drawFart();
+                this.pos.y += this.vel.y;
+                this.vel.y *= 0.99; 
+                if (this.vel.y > - 0.5) {
+                    this.vel.y = - 0.5;
+                }
+                this.fartPos.y += this.fartVel.y;
+                this.fartVel.y *= 0.96;
+                if (this.fartVel.y < 0.5 && this.fartPos.y < this.canvas.height - 100) {
+                    this.fartVel.y = 0.5;
+                }
+                this.fartSize.w += this.fartVel.y * 2;
+                this.fartSize.h += this.fartVel.y * 2;
             }
-            this.fartPos.y += this.fartVel.y;
-            this.fartVel.y *= 0.96;
-            if (this.fartVel.y < 0.5 && this.fartPos.y < this.canvas.height - 100) {
-                this.fartVel.y = 0.5;
-            }
-            this.fartSize.w += this.fartVel.y * 2;
-            this.fartSize.h += this.fartVel.y * 2;
         }
         if (this.gameArea.state === 'winning') {
-            this.ctx.drawImage(this.winEff, this.pos.x - this.size.w/2, this.pos.y - this.size.h/2, 200, 283);
+            this.ctx.drawImage(this.winEff, this.pos.x - 100, this.pos.y - 141.5, 200, 283);
         } else {
             this.ctx.drawImage(this.eff, this.pos.x - this.size.w/2, this.pos.y - this.size.h/2, this.size.w, this.size.h);
         } 
@@ -192,19 +238,13 @@ class Player {
         this.ctx.drawImage(this.fart, this.fartPos.x - this.fartSize.w/2 + 20, this.fartPos.y - this.fartSize.h/2, this.fartSize.w, this.fartSize.h);
     }
 
-    checkCollision() {
-        GameArea.updatePoints();
-    }
-
-    updateLives() {
-        if(this.lives > 0) {this.lives -= 1; }
+    drawLoseText() {
+            this.ctx.drawImage(this.loseTextEff, this.canvas.width/2 - 208, this.canvas.height/2 - 142, 416, 284);
     }
 
     isDead() {
         return lives <= 0;
     }
-
-
 
 }
 
@@ -221,7 +261,6 @@ class Projectile {
     }
 
     drawProjectile() {
-    
         this.ctx.drawImage(this.eff, this.pos.x, this.pos.y, this.size.w, this.size.h);
     }
 
@@ -229,10 +268,6 @@ class Projectile {
         this.drawProjectile();
         this.pos.x = this.pos.x + this.vel.x;
         this.pos.y = this.pos.y + this.vel.y;
-    }
-
-    checkCollision() {
-        
     }
 
 }
@@ -266,6 +301,37 @@ class Enemy {
     }
 
 
+}
+
+class Win {
+    constructor (gameArea, velocity) {
+        this.gameArea = gameArea;
+        this.canvas = this.gameArea.canvas;
+        this.ctx = this.gameArea.ctx;
+        this.size = {w: 60, h: 46};
+        this.pos = {x: gameArea.size.w/2, y: gameArea.size.h/2};
+        this.vel = velocity;
+        this.eff = new Image();
+        this.num = Math.floor(Math.random() * 8 + 1);
+        this.eff.src = `./images/win${this.num}.png`;
+        this.winTextEff = new Image();
+        this.winTextEff.src = './images/you-win-2.png';
+    }
+
+    drawWin() {
+        this.ctx.drawImage(this.eff, this.pos.x - 40, this.pos.y - 70.75, this.size.w, this.size.h);
+        this.drawWinText();
+    }
+
+    drawWinText() {
+        this.ctx.drawImage(this.winTextEff, this.canvas.width/2 - 213.5, this.canvas.height/2 + 60, 427, 87);
+    }
+
+    update() {
+        this.drawWin();
+        this.pos.x = this.pos.x + this.vel.x;
+        this.pos.y = this.pos.y + this.vel.y;
+    }
 }
 
 class Boss {
