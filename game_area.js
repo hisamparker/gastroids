e2cf1fd5f33e202aaa2c8abdb0b0ef7a26a189aa
hisamparker <hisamparker@gmail.com>
2021-04-20 09:05:25 +0200

    fixed reset bug with raf not cancelling and added block/warning for screens that are too small to play

--- game_area.js
+++ game_area.js
@@ -239,6 +239,7 @@ class GameArea {
     }
 
     resetGame(){
+        this.stopGame();
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.state.frames = 0;
         this.state.level = 1;
